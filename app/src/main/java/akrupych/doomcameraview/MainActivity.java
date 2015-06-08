ee5a399b93bdc949f7e0b63f32f4a8335a2472c7
Andrii Krupych <andriy.krupych@gmail.com>
2015-06-09 02:43:03 +0300

    Added main fragment

--- app/src/main/java/akrupych/doomcameraview/MainActivity.java
+++ app/src/main/java/akrupych/doomcameraview/MainActivity.java
@@ -1,12 +1,9 @@
 package akrupych.doomcameraview;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/akrupych/doomcameraview/MainFragment.java
@@ -0,0 +1,20 @@
+package akrupych.doomcameraview;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Andrew on 6/9/2015.
+ */
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
